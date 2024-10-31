d5574936da89e9f99ae62216d8d84daf0033b7d8
alexander shen <alexander.shen@gmail.com>
2024-10-30 22:24:47 -0400

    stopped logging of environment variables in frontend

--- src/App.js
+++ src/App.js
@@ -22,9 +22,6 @@ import theme from "./theme";
 import { ThemeProvider } from "@mui/material/styles";
 
 const App = () => {
-  console.log(process.env);
-  console.log('ENV:', process.env.REACT_APP_ENV);
-
   return (
     <ThemeProvider theme={theme}>
       <AuthProvider>
